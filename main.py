7ab009adf1d131190225ffbb40da3ced480ade7b
Baskul89 <wowspec2010@mail.ru>
2024-10-26 20:37:37 +0400

    Test commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
+
+a = 1
+b = 2
+a,b = b,a
+print(a, b)
+name = ''
+if (name == 'Urban') or (name == 'Gordey') :
+    print(1)
+elif name[-1] == '1':
+    print(2)
+else :
+    print()
+
+
+
