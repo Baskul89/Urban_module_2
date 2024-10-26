d1a348648b38e3aa4af18ad2a8b8144b794e630b
Baskul89 <wowspec2010@mail.ru>
2024-10-26 20:37:37 +0400

    New commit message

--- main.py
+++ main.py
@@ -4,17 +4,10 @@ y = 32
 print(x * y)
 print("End line")
 
-a = 1
-b = 2
-a,b = b,a
-print(a, b)
-name = ''
-if (name == 'Urban') or (name == 'Gordey') :
-    print(1)
-elif name[-1] == '1':
-    print(2)
-else :
-    print()
+string = 'Строка'
 
+def is_vowels(number):
+    vowels = ['а', 'е']
+    return number in vowels
 
-
+print(string)
